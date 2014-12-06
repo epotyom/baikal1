39fae75e9d971a8cdce3336c51e709508d7d1f80
Egor Potiomkin <eg13reg@gmail.com>
2014-12-06 12:20:32 +0800

    changes

--- app/about/about.js
+++ app/about/about.js
@@ -4,14 +4,18 @@ angular.module('baikalApp.about', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .when('/about/:category', {
-    templateUrl: 'post/list.html',
-    controller: 'PostlistCtrl',
-  })
   .when('/post/:pid', {
     templateUrl: 'post/view.html',
     controller: 'PostviewCtrl',
   });
+  .when('/about/:category', {
+    templateUrl: 'post/list.html',
+    controller: 'PostlistCtrl',
+  })  
+  .when('/contacts', {
+    templateUrl: 'post/view.html',
+    controller: 'ContactsCtrl',
+  })
 }])
 
 .controller('PostlistCtrl', ['$scope', '$routeParams', 'Post', 'Category', function($scope, $routeParams, Post, Category) {
@@ -21,6 +25,10 @@ angular.module('baikalApp.about', ['ngRoute'])
 	$scope.posts = Post.query({pid: $routeParams.category});
 }])
 
-.controller('PostviewCtrl', ['$scope', '$routeParams', 'Post', 'Category', function($scope, $routeParams, Post, Category) {
+.controller('PostviewCtrl', ['$scope', '$routeParams', 'Post', function($scope, $routeParams, Post) {
 	$scope.post = Post.get({pid: $routeParams.pid});
+}])
+
+.controller('ContactsCtrl', ['$scope', 'Post', function($scope, $routeParams, Post) {
+  $scope.post = Post.get({pid: "contacts"});
 }]);
