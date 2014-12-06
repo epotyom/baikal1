894881fd8ee3b4c852a0297556221d17ce4c98b9
Egor Potiomkin <eg13reg@gmail.com>
2014-12-06 18:31:47 +0800

    changes

--- app/js/app.js
+++ app/js/app.js
@@ -65,4 +65,5 @@ config(['$routeProvider', function($routeProvider) {
     BAIKAL_JOURNEY_BUTTON: 'GO TO JOURNEY',
   });
   $translateProvider.preferredLanguage('ru');
+  $translateProvider.rememberLanguage(true);
 });
