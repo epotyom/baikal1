58c142810a88a24c2cfd82d206ae104c92d29d0c
Egor Potiomkin <eg13reg@gmail.com>
2014-12-06 17:21:17 +0800

    changes

--- app/js/app.js
+++ app/js/app.js
@@ -24,10 +24,37 @@ config(['$routeProvider', function($routeProvider) {
     TITLE: 'Лига Байкальских Капитанов',
     COPYRIGHT_TEXT: 'Все права защищены',
     COPYRIGHT_AUTHOR: 'Александр Бурмейстер',
-    
+    PHOTO: 'Фото',
+    SPRING: 'Весна',
+    SUMMER: 'Лето',
+    AUTUMN: 'Осень',
+    WINTER: 'Зима',
+    ABOUT_US: 'О нас',
+    FAMILY: 'Семья',
+    MEDIA: 'СМИ',
+    CREATIVE: 'Творчество',
+    YOGA: 'Йога',
+    OUR_GUESTS: 'Наши гости',
+    JOURNEY: 'Путешествия',
+    CONTACTS: 'Контакты',
   });
   $translateProvider.translations('en', {
-    TITLE: 'Baikal Captain Ligue'
+    TITLE: 'Baikal Captain Ligue',
+    COPYRIGHT_TEXT: 'All rights reserved',
+    COPYRIGHT_AUTHOR: 'Alexander Burmeister',
+    PHOTO: 'Photo',
+    SPRING: 'Spring',
+    SUMMER: 'Summer',
+    AUTUMN: 'Autumn',
+    WINTER: 'Winter',
+    ABOUT_US: 'About us',
+    FAMILY: 'Family',
+    MEDIA: 'Media',
+    CREATIVE: 'Creative',
+    YOGA: 'Yoga',
+    OUR_GUESTS: 'Our guests',
+    JOURNEY: 'Journey',
+    CONTACTS: 'Contacts',
   });
   $translateProvider.preferredLanguage('ru');
 });
