c2cb9727327eadd0a946b9895d661711062c9841
Egor Potiomkin <eg13reg@gmail.com>
2015-01-01 21:46:18 +0800

    music

--- app/js/services.js
+++ app/js/services.js
@@ -9,7 +9,7 @@ angular.module('baikalApp.services', ['ngResource'])
 
 .factory('Music', ['$resource',
   function($resource){
-    var Music = {state: 'pause', current: 0};
+    var Music = {state: 0 /* 0 - pause, 1 - play, 2 - loading */, current: 0};
     Music.list = $resource('music/list.json', {}, {}).query(function() {
       for (var i = Music.list.length - 1; i >= 0; i--) {
         Music.list[i].id = i;
@@ -20,22 +20,22 @@ angular.module('baikalApp.services', ['ngResource'])
           Music.playNext();
       })
       .bind('playing', function(){
-          console.log(Music.state+' -> play');
-          Music.state = 'play';
+          console.log(Music.state+' -> 1');
+          Music.state = 1;
       })      
       .bind('pause', function(){
-        console.log(Music.state+' -> pause');
-          Music.state = 'pause';
+        console.log(Music.state+' -> 0');
+          Music.state = 0;
       })
       .bind('waiting', function(){
-        console.log(Music.state+' -> loading');
-          Music.state = 'loading';
+        console.log(Music.state+' -> 2');
+          Music.state = 2;
       });
     });
 
     Music.playPause = function(id) {
       if (typeof id == 'undefined' || id == Music.current) {
-        if (Music.state == 'play') {
+        if (Music.state == 1) {
           document.getElementById('audio_audio').pause();
         } else {
           document.getElementById('audio_audio').play();
