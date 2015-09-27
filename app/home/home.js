dbe597660d0894ac63cae7e15a661cab47c87344
Egor Potiomkin <eg13reg@gmail.com>
2015-09-28 01:54:08 +0800

    main button season change

--- app/home/home.js
+++ app/home/home.js
@@ -5,23 +5,42 @@ angular.module('baikalApp.home', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
+    controller: 'HomeCtrl',
+  })
+  .when('/home-summer', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl',
+    data: 'summer'
+  })  
+  .when('/home-winter', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl',
+    data: 'winter'
   });
 }])
 
-.controller('HomeCtrl', ['$scope',function($scope) {
+.controller('HomeCtrl', ['$scope', '$route', function($scope, $route) {
 	$scope.home_background = {
-		'background': 'url(images/bg_header.jpg) no-repeat top center fixed',
 		'-webkit-background-size': 'cover',
 		'-moz-background-size': 'cover',
 		'-o-background-size': 'cover',
 		'background-size': 'cover',
 	};
 	var curmonth = new Date()
-	if (curmonth < 4 || curmonth > 11) {
+	var is_winter = false;
+	if ($route.current) {
+		if ($route.current.data == 'winter') {
+			is_winter = true;
+		}
+	} else if (curmonth < 4 || curmonth > 11) {
+		is_winter = true;
+	}
+	if (is_winter) {
 		$scope.journey_button_img = 'jeep';
+		$scope.home_background['background'] = 'url(images/bg_header_winter.jpg) no-repeat top center fixed';
 	} else {
 		$scope.journey_button_img = 'sailboat';
+		$scope.home_background['background'] = 'url(images/bg_header_summer.jpg) no-repeat top center fixed';
 	}
 	
 	//jQuery('#mainButton').popover();
